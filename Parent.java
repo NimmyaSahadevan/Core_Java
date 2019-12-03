fae5b94bf71c87947294d7324ba0116db1f5a961
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2019-12-03 09:17:27 +0530

    Add files via upload

--- Parent.java
+++ Parent.java
@@ -8,27 +8,22 @@ class Child {
       z = x + y;
       System.out.println("The sum of the given numbers:"+z);
    }
-	
-  
 }
 
  class Parent extends Child {
    public void addition(int x, int y) {
-      z = x + y;
+      z = x + y+10;
       System.out.println("The sum of the given numbers:"+z);
    }
 	
    public static void main(String args[]) {
-     
-	   Scanner sc=new Scanner(System.in);
+      Scanner sc=new Scanner(System.in);
 		System.out.println("Enter first number");
 		int a=sc.nextInt();
 		System.out.println("Enter second number");
 		int b=sc.nextInt();
-      Child obj = new Parent();
-      obj.addition(a, b);
-      sc.close();
-     
+        Child obj = new Parent();
+        obj.addition(a,b);
    }
 }
    
