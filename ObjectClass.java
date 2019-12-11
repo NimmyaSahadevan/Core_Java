ea676a29bfe2546dc8f71fed31e34f559680c188
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2019-12-11 15:57:51 +0530

    Add files via upload

---
+++ ObjectClass.java
@@ -0,0 +1,37 @@
+
+public class ObjectClass {
+
+	
+	static int last_roll=100;
+	int roll_no;
+	
+	ObjectClass()          // constructor
+	{
+	roll_no=last_roll;
+	last_roll++;
+	}
+	
+	public int hashCode()
+	{
+		return roll_no;
+	}
+	 public static void main(String args[])     // main
+	 {
+		 
+		 ObjectClass s=new ObjectClass();     
+		 System.out.println(s);
+		 System.out.println(s.toString());
+		 Object obj=new String("FACE");
+		 Class c=obj.getClass();
+		 System.out.println("Class of Object obj is :"+c.getName());
+		 s=null;
+		 System.gc();     // garbage collection
+		 
+		 
+	 }
+	protected void finalize()
+	{
+	 System.out.println("Finalize method called");
+	}
+	
+}
