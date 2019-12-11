bd8799e338ad09a28d4386e8da0a503aec89e2f1
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2019-12-11 10:48:37 +0530

    Add files via upload

---
+++ StaticExample.java
@@ -0,0 +1,25 @@
+
+
+
+//  STATIC VARIABLE
+
+
+
+
+public class StaticExample {
+	
+	static int c=0;
+	StaticExample()
+	{
+		c++;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticExample c1=new StaticExample();
+		StaticExample c2=new StaticExample();
+		StaticExample c3=new StaticExample();
+	}
+
+}
