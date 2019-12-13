7f53c366a5dc022c8dfcfe75c790a0bb49138801
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2019-12-13 16:12:06 +0530

    Program for AutoBoxing

--- AutoBoxing.java
+++ AutoBoxing.java
@@ -1,7 +1,9 @@
 
-public class AutoBoxing {
+public class AutoBoxing 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		Integer i=20;    // converting int into Integer
 		Integer j=i;     // Autoboxing now compiler will write Integer.valueOf(a) internally
