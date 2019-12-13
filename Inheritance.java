f36619e087d6962b23023da6d47013c1975a7f03
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2019-12-13 16:10:01 +0530

    Program for Inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -1,27 +1,31 @@
 
 import java.util.Scanner;
 
-class Child {
+class Child
+{
    int z;
 	
-   public void addition(int x, int y) {
+   public void addition(int x, int y)
+   {
       z = x + y;
       System.out.println("The sum of the given numbers:"+z);
    }
 }
-
- class Parent extends Child {
-   public void addition(int x, int y) {
+class Parent extends Child
+{
+   public void addition(int x, int y)
+   {
       z = x + y+10;
       System.out.println("The sum of the given numbers:"+z);
    }
 	
-   public static void main(String args[]) {
-      Scanner sc=new Scanner(System.in);
-		System.out.println("Enter first number");
-		int a=sc.nextInt();
-		System.out.println("Enter second number");
-		int b=sc.nextInt();
+   public static void main(String args[]) 
+   {
+      	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter first number");
+	int a=sc.nextInt();
+	System.out.println("Enter second number");
+	int b=sc.nextInt();
         Child obj = new Parent();
         obj.addition(a,b);
    }
