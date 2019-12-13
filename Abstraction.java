15ebedac3b262fc886b983e9b718059bf1175090
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2019-12-13 18:04:23 +0530

    Example Program for Abstraction

--- Abstraction.java
+++ Abstraction.java
@@ -1,7 +1,9 @@
 
-public class Abstraction {
+public class Abstraction 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		
 		Racoon r=new Racoon("Racoon");
