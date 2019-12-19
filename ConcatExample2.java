877d7ea86c17544d2f4c7245f095696e30f9448a
NimmyaSahadevan <58411427+NimmyaSahadevan@users.noreply.github.com>
2019-12-19 12:11:51 +0530

    Program using String concat()

--- ConcatExample2.java
+++ ConcatExample2.java
@@ -6,7 +6,7 @@ public class ConcatExample2
 		String s1="Hello";
 		String s2="Face@2.0";
 		String s3="Reader";
-		String s4=s1.concat(s2);  //concatenating one string
+		String s4=s1.concat(s2);             //concatenating one string
 		System.out.println(s4);
 		String s5=s1.concat(s2).concat(s3);  //concatenating multiple string
 		System.out.println(s5);
